2c2f35cede3431b021c521e089026f9bbd1e1221
kumarprashant24 <kumar.prashant@tothenew.com>
2023-01-13 16:04:26 +0530

    changes in config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export let API_URL;
 
 if (process.env.REACT_APP_STAGE === 'prod') {
-  API_URL = 'https://chatgpt-8uek.onrender.com';
+  API_URL = 'https://chatgpt-7mkc.onrender.com';
 }
 if (process.env.REACT_APP_STAGE === 'dev') {
   API_URL = 'http://localhost:5000';
